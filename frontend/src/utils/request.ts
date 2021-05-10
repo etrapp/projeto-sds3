6ec362520fda4771e0f316c8f46097225756e476
etrapp <etrapp@gmail.com>
2021-05-10 00:04:38 -0300

    Env var

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.BACKEND_URL ?? 'https://sds3-emiliano.herokuapp.com';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds3-emiliano.herokuapp.com';
